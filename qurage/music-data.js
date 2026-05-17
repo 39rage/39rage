1b00aceda7bf35187fb77c47669ad168d5ca855e
39rage <39rage@gmail.com>
2026-05-17 20:39:15 +0900

    music-data.js

---
+++ qurage/music-data.js
@@ -0,0 +1,21 @@
+// 【全アルバムのリスト】
+const allAlbums = [
+    {
+        id: "first-step", 
+        title: "First step",
+        subtitle: "1st Mini Album",
+        img: "https://fromtheasia.com/wp-content/uploads/NCG63.jpg",
+        desc: "初めて作った5曲がまとまってます。<br>デモ曲のようなミニアルバム。",
+        booth: "https://casbgcasbg.booth.pm/items/2691899",
+        tracksCount: "5 Tracks"
+    }
+];
+
+// 【全曲のリスト】
+const allTracks = [
+    { title: "British Brixton", file: "2020102923 British_Brixton-100_C.mp3", albumId: "first-step" },
+    { title: "Accelerate Afghanistan", file: "2020103012 Accelerate_Afghanistan_TH-102_Cm.mp3", albumId: "first-step" },
+    { title: "AABS 80s", file: "2020103014 AABS_80s-128_Cm.mp3", albumId: "first-step" },
+    { title: "Altered Afterburner AYB", file: "2020103018 Altered_Afterburner_AYB-129_Csm.mp3", albumId: "first-step" },
+    { title: "Alternating synth layers", file: "2020103111 Alternating_synth_layers-125_Dm.mp3", albumId: "first-step" }
+];
